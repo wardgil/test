bfeea7225eea524232836d039b94da8ed31b5205
wardgil <522285875@qq.com>
2020-04-23 12:05:53 +0800

    测试

---
+++ src/com/test1/Test.java
@@ -0,0 +1,13 @@
+package com.test1;
+
+/**
+ * test
+ *
+ * @Author: wwb
+ * @Date: 2020/4/23 11:56
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
